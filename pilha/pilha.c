a891af89360146a8672dbabc80c895e32e58a8c4
pamplona-c <gustavo.pamplona.549@gmail.com>
2023-08-01 11:12:02 -0300

    Pilhas

---
+++ pilha/pilha.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//declara item
+typedef struct{
+    int item;
+}pilhaItem;
+
+//inicializa a pilha
+int topo = -1;
+
+//função incrementar pilha
+void incrementaPilha(pilhaItem p[5],int item){
+    if (topo >= 4)
+    {
+        printf("Não é possível adicionar items na pilha.");
+        return;
+    }
+    topo++;
+    p[topo].item = item;
+}
+
+void decrementa(pilhaItem p[5]){
+    p[topo].item = 0;
+    topo--;
+}
+
+
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    //declara a pilha
+    
+    pilhaItem p[5];
+    int esc = 0,x = 0;
+    
+    while (esc != 3)
+    {
+        printf("\n---PILHA---\n");
+        printf("1 - INCREMENTAR PILHA.\n");
+        printf("2 - DECREMENTAR PILHA.\n");
+        printf("3 - SAIR\n");
+
+        scanf("%d",&esc);
+
+        switch (esc)
+        {
+        case 1:
+            if (topo >= 4)
+            {
+                printf("\nNão é possível adicionar items na pilha.\n");
+                break;
+            }
+            
+            printf("Digite o valor a ser inserido na pilha:\n");
+            scanf("%d",&x);
+            incrementaPilha(p,x);
+            printf("\nTopo = %d\n",topo);
+            break;
+        
+        case 2:
+            if (topo < 1)
+            {
+                printf("\nA pilha ja esta vazia.\n");
+                break;
+            }
+            
+            decrementa(p);
+            printf("\nTopo = %d\n",topo);
+            break;
+        }
+    }
+    
+    return 0;
+}
