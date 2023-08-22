9190732c7e78c9bc6c4b4c80f814fcca00d4b19a
pamplona-c <gustavo.pamplona.549@gmail.com>
2023-08-22 13:51:25 -0300

    adição de exercício lista 02

---
+++ pilha/Lista_2/01.c
@@ -0,0 +1,15 @@
+/*Digamos que nosso alfabeto contém apenas as letras a, b e c. Qualquer string desse
+conjunto tem a forma WcM, onde W é uma sequência das letras ab, e M é o inverso de
+W. Por exemplo: aabababcbababaa. Escreva um programa que decida se uma
+determinada string pertence ou não a forma WcM. Utilize uma estrutura de pilha para
+resolver o problema.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main(){
+    
+    return 0;
+}
