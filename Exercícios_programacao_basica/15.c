dc08be3a12e27d3cb6c28378202cacfa8cbfedc8
pamplona-c <gustavo.pamplona.549@gmail.com>
2023-08-22 09:45:55 -0300

    Adição de exercício 15

---
+++ Exercícios_programacao_basica/15.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//Faça um programa que receba dois números X e Y, sendo X < Y. Calcule e mostre:
+//A. A soma dos números pares desse intervalo de números, incluindo os números digitados;
+
+//B. A multiplicação dos números ímpares desse intervalo, incluindo os digitados;
+
+void soma_pares(){
+
+}
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+    int x = 0,y = 0;
+    printf("Digite Dois números X e Y, sendo X < Y :\n");
+    scanf("%d %d",&x,&y);
+
+    if (x > y)
+    {
+        printf("")
+    }
+    
+
+
+    return 0;
+}
